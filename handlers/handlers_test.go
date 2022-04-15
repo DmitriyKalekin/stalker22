8b75c2ac6d921de7d7f134c77a332f76991e16db
Dmitriy Kalekin <herrhorror@gmail.com>
2022-04-15 15:06:07 +0600

    structured test-cases

--- handlers/handlers_test.go
+++ handlers/handlers_test.go
@@ -25,47 +25,50 @@ func typeof(v interface{}) string {
 }
 
 func TestTgWebhookHandler(t *testing.T) {
-	// --- given --------
-	json_response := `{"update_id":464725880, "message":{"message_id":11,"from":{"id":435627225,"is_bot":false,"first_name":"Дмитрий","last_name":"Калекин","username":"herr_horror","language_code":"en"},"chat":{"id":435627225,"first_name":"Дмитрий","last_name":"Калекин","username":"herr_horror","type":"private"},"date":1650009613,"text":"привет"}}`
-	r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
 
-	// Create a request to pass to our handler
-	req, err := http.NewRequest("POST", "/tg"+TELEGRAM_TOKEN, r)
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("200 OK on correct telegram JSON-request", func(t *testing.T) {
+		// --- given --------
+		json_response := `{"update_id":464725880, "message":{"message_id":11,"from":{"id":435627225,"is_bot":false,"first_name":"Дмитрий","last_name":"Калекин","username":"herr_horror","language_code":"en"},"chat":{"id":435627225,"first_name":"Дмитрий","last_name":"Калекин","username":"herr_horror","type":"private"},"date":1650009613,"text":"привет"}}`
+		r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
 
-	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(TgHandler)
+		// Create a request to pass to our handler
+		req, err := http.NewRequest("POST", "/tg"+TELEGRAM_TOKEN, r)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
-	// directly and pass in our Request and ResponseRecorder.
-	handler.ServeHTTP(rr, req)
+		// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(TgHandler)
 
-	// Check the status code is what we expect.
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusOK)
-	}
+		// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+		// directly and pass in our Request and ResponseRecorder.
+		handler.ServeHTTP(rr, req)
 
-	// log.Warn(rr.HeaderMap.Values("Content-Type"))
+		// Check the status code is what we expect.
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
 
-	// Check the response body is what we expect.
-	expected := Response{Status: "OK"} //`{"status":"OK"}`
+		// log.Warn(rr.HeaderMap.Values("Content-Type"))
 
-	var got_value Response
-	json.NewDecoder(rr.Body).Decode(&got_value)
+		// Check the response body is what we expect.
+		expected := Response{Status: "OK"} //`{"status":"OK"}`
 
-	assert.EqualValues(t, typeof(Response{}), typeof(got_value))
-	assert.EqualValues(t, "OK", got_value.Status)
-	assert.EqualValues(t, expected, got_value)
+		var got_value Response
+		json.NewDecoder(rr.Body).Decode(&got_value)
 
-	// if rr.Body.String() != expected {
-	// 	log.Warn([]byte(expected))
-	// 	log.Warn([]byte(rr.Body.Bytes()))
-	// 	t.Errorf("handler returned unexpected body: got '%v' want '%v'",
-	// 		rr.Body.String(), expected)
-	// }
+		assert.EqualValues(t, typeof(Response{}), typeof(got_value))
+		assert.EqualValues(t, "OK", got_value.Status)
+		assert.EqualValues(t, expected, got_value)
+
+		// if rr.Body.String() != expected {
+		// 	log.Warn([]byte(expected))
+		// 	log.Warn([]byte(rr.Body.Bytes()))
+		// 	t.Errorf("handler returned unexpected body: got '%v' want '%v'",
+		// 		rr.Body.String(), expected)
+		// }
+	})
 
 }
--- telegram_api_client/telegram_api_client_test.go
+++ telegram_api_client/telegram_api_client_test.go
@@ -37,178 +37,186 @@ func typeof(v interface{}) string {
 	return fmt.Sprintf("%T", v)
 }
 
-func TestWrongTokenOnGetWebhookInfo(t *testing.T) {
-	// --- given --------
-	json_response := `{"description":"Not Found","error_code":404,"ok":false}`
-
-	telegram_client := NewClient("WRONG_TOKEN")
-	telegram_client.HttpClient = &MockClient{
-		GetFunc: func(url string) (*http.Response, error) {
-			r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
-			return &http.Response{
-				StatusCode: http.StatusNotFound,
-				Body:       r,
-			}, nil
-		},
-	}
-
-	// --- try --------
-	info, err := telegram_client.GetWebhookInfo()
-
-	// --- assert --------
-	assert.NotNil(t, info)
-	assert.Nil(t, err)
-	assert.EqualValues(t, typeof(APIResponse{}), typeof(info))
-	assert.EqualValues(t, false, info.Ok)
-	assert.EqualValues(t, http.StatusNotFound, info.ErrorCode)
-
-}
-
 func TestGetWebhookInfo(t *testing.T) {
-	// --- given --------
-	json_response := `{"ok":true,"result":{"url":"","has_custom_certificate":false,"pending_update_count":0}}`
 
-	telegram_client := NewClient("CORRECT_TELEGRAM_TOKEN")
-	telegram_client.HttpClient = &MockClient{
-		GetFunc: func(url string) (*http.Response, error) {
-			r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
-			return &http.Response{
-				StatusCode: http.StatusOK,
-				Body:       r,
-			}, nil
-		},
-	}
+	t.Run("404 on wrong token", func(t *testing.T) {
+		// --- given --------
+		json_response := `{"description":"Not Found","error_code":404,"ok":false}`
 
-	// --- try --------
-	info, err := telegram_client.GetWebhookInfo()
-	var result WebhookInfo
-	json.Unmarshal(*info.Result, &result)
+		telegram_client := NewClient("WRONG_TOKEN")
+		telegram_client.HttpClient = &MockClient{
+			GetFunc: func(url string) (*http.Response, error) {
+				r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
+				return &http.Response{
+					StatusCode: http.StatusNotFound,
+					Body:       r,
+				}, nil
+			},
+		}
 
-	// --- assert --------
-	assert.NotNil(t, info)
-	assert.Nil(t, err)
-	assert.EqualValues(t, typeof(APIResponse{}), typeof(info))
-	assert.EqualValues(t, true, info.Ok)
+		// --- try --------
+		info, err := telegram_client.GetWebhookInfo()
 
-	assert.EqualValues(t, "", result.Url)
-	assert.EqualValues(t, false, result.HasCustomCertificate)
-	assert.EqualValues(t, []string(nil), result.AllowedUpdates)
+		// --- assert --------
+		assert.NotNil(t, info)
+		assert.Nil(t, err)
+		assert.EqualValues(t, typeof(APIResponse{}), typeof(info))
+		assert.EqualValues(t, false, info.Ok)
+		assert.EqualValues(t, http.StatusNotFound, info.ErrorCode)
+	})
+
+	t.Run("200 OK on correct token", func(t *testing.T) {
+		// --- given --------
+		json_response := `{"ok":true,"result":{"url":"","has_custom_certificate":false,"pending_update_count":0}}`
+
+		telegram_client := NewClient("CORRECT_TELEGRAM_TOKEN")
+		telegram_client.HttpClient = &MockClient{
+			GetFunc: func(url string) (*http.Response, error) {
+				r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       r,
+				}, nil
+			},
+		}
+
+		// --- try --------
+		info, err := telegram_client.GetWebhookInfo()
+		var result WebhookInfo
+		json.Unmarshal(*info.Result, &result)
+
+		// --- assert --------
+		assert.NotNil(t, info)
+		assert.Nil(t, err)
+		assert.EqualValues(t, typeof(APIResponse{}), typeof(info))
+		assert.EqualValues(t, true, info.Ok)
+
+		assert.EqualValues(t, "", result.Url)
+		assert.EqualValues(t, false, result.HasCustomCertificate)
+		assert.EqualValues(t, []string(nil), result.AllowedUpdates)
+
+	})
+
 }
 
-func TestWrongTokenOnSetWebhook(t *testing.T) {
-	// --- given --------
-	json_response := `{"description":"Not Found","error_code":404,"ok":false}`
+func TestSetWebhook(t *testing.T) {
 
-	telegram_client := NewClient("WRONG_TOKEN")
-	telegram_client.HttpClient = &MockClient{
-		GetFunc: func(url string) (*http.Response, error) {
-			r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
-			return &http.Response{
-				StatusCode: http.StatusNotFound,
-				Body:       r,
-			}, nil
-		},
-	}
+	t.Run("404 on wrong token", func(t *testing.T) {
+		// --- given --------
+		json_response := `{"description":"Not Found","error_code":404,"ok":false}`
 
-	// --- try --------
-	url := "http://localhost:3000/tg"
-	info, err := telegram_client.SetWebhook(url)
+		telegram_client := NewClient("WRONG_TOKEN")
+		telegram_client.HttpClient = &MockClient{
+			GetFunc: func(url string) (*http.Response, error) {
+				r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
+				return &http.Response{
+					StatusCode: http.StatusNotFound,
+					Body:       r,
+				}, nil
+			},
+		}
 
-	// --- assert --------
-	assert.NotNil(t, info)
-	assert.Nil(t, err)
-	assert.EqualValues(t, typeof(APIResponse{}), typeof(info))
-	assert.EqualValues(t, false, info.Ok)
-	assert.EqualValues(t, http.StatusNotFound, info.ErrorCode)
-}
+		// --- try --------
+		url := "http://localhost:3000/tg"
+		info, err := telegram_client.SetWebhook(url)
 
-func TestNoHTTPSOnSetWebhook(t *testing.T) {
-	// --- given --------
-	json_response := `{"description":"Bad Request: bad webhook: HTTPS url must be provided for webhook", "error_code":400, "ok":false}`
+		// --- assert --------
+		assert.NotNil(t, info)
+		assert.Nil(t, err)
+		assert.EqualValues(t, typeof(APIResponse{}), typeof(info))
+		assert.EqualValues(t, false, info.Ok)
+		assert.EqualValues(t, http.StatusNotFound, info.ErrorCode)
+	})
 
-	telegram_client := NewClient("CORRECT_TOKEN")
-	telegram_client.HttpClient = &MockClient{
-		GetFunc: func(url string) (*http.Response, error) {
-			r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
-			return &http.Response{
-				StatusCode: http.StatusBadRequest,
-				Body:       r,
-			}, nil
-		},
-	}
+	t.Run("400 bad request on no-HTTPS support", func(t *testing.T) {
+		// --- given --------
+		json_response := `{"description":"Bad Request: bad webhook: HTTPS url must be provided for webhook", "error_code":400, "ok":false}`
 
-	// --- try --------
-	url := "http://localhost:3000/tg"
-	info, err := telegram_client.SetWebhook(url)
+		telegram_client := NewClient("CORRECT_TOKEN")
+		telegram_client.HttpClient = &MockClient{
+			GetFunc: func(url string) (*http.Response, error) {
+				r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
+				return &http.Response{
+					StatusCode: http.StatusBadRequest,
+					Body:       r,
+				}, nil
+			},
+		}
 
-	// --- assert --------
-	assert.NotNil(t, info)
-	assert.Nil(t, err)
-	assert.EqualValues(t, typeof(APIResponse{}), typeof(info))
-	assert.EqualValues(t, false, info.Ok)
-	assert.EqualValues(t, http.StatusBadRequest, info.ErrorCode)
-}
+		// --- try --------
+		url := "http://localhost:3000/tg"
+		info, err := telegram_client.SetWebhook(url)
 
-func TestWasSetWebhook(t *testing.T) {
-	// --- given --------
-	json_response := `{"description":"Webhook was set", "ok":true, "result":true}`
+		// --- assert --------
+		assert.NotNil(t, info)
+		assert.Nil(t, err)
+		assert.EqualValues(t, typeof(APIResponse{}), typeof(info))
+		assert.EqualValues(t, false, info.Ok)
+		assert.EqualValues(t, http.StatusBadRequest, info.ErrorCode)
+	})
 
-	telegram_client := NewClient("CORRECT_TOKEN")
-	telegram_client.HttpClient = &MockClient{
-		GetFunc: func(url string) (*http.Response, error) {
-			r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
-			return &http.Response{
-				StatusCode: http.StatusOK,
-				Body:       r,
-			}, nil
-		},
-	}
+	t.Run("200 OK was set", func(t *testing.T) {
+		// --- given --------
+		json_response := `{"description":"Webhook was set", "ok":true, "result":true}`
 
-	// --- try --------
-	url := "https://15ba-217-25-217-143.eu.ngrok.io/tg"
-	info, err := telegram_client.SetWebhook(url)
-	var result bool
-	json.Unmarshal(*info.Result, &result)
+		telegram_client := NewClient("CORRECT_TOKEN")
+		telegram_client.HttpClient = &MockClient{
+			GetFunc: func(url string) (*http.Response, error) {
+				r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       r,
+				}, nil
+			},
+		}
 
-	// --- assert --------
-	assert.NotNil(t, info)
-	assert.Nil(t, err)
-	assert.EqualValues(t, typeof(APIResponse{}), typeof(info))
-	assert.EqualValues(t, true, info.Ok)
-	assert.EqualValues(t, "Webhook was set", info.Description)
-	assert.EqualValues(t, 0, info.ErrorCode)
+		// --- try --------
+		url := "https://15ba-217-25-217-143.eu.ngrok.io/tg"
+		info, err := telegram_client.SetWebhook(url)
+		var result bool
+		json.Unmarshal(*info.Result, &result)
 
-	assert.EqualValues(t, true, result)
-}
+		// --- assert --------
+		assert.NotNil(t, info)
+		assert.Nil(t, err)
+		assert.EqualValues(t, typeof(APIResponse{}), typeof(info))
+		assert.EqualValues(t, true, info.Ok)
+		assert.EqualValues(t, "Webhook was set", info.Description)
+		assert.EqualValues(t, 0, info.ErrorCode)
 
-func TestAlreadySetWebhook(t *testing.T) {
-	// --- given --------
-	json_response := `{"ok":true,"description":"Webhook is already set","result":true}`
+		assert.EqualValues(t, true, result)
 
-	telegram_client := NewClient("CORRECT_TOKEN")
-	telegram_client.HttpClient = &MockClient{
-		GetFunc: func(url string) (*http.Response, error) {
-			r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
-			return &http.Response{
-				StatusCode: http.StatusOK,
-				Body:       r,
-			}, nil
-		},
-	}
+	})
 
-	// --- try --------
-	url := "https://15ba-217-25-217-143.eu.ngrok.io/tg"
-	info, err := telegram_client.SetWebhook(url)
-	var result bool
-	json.Unmarshal(*info.Result, &result)
+	t.Run("200 OK already set", func(t *testing.T) {
+		// --- given --------
+		json_response := `{"ok":true,"description":"Webhook is already set","result":true}`
 
-	// --- assert --------
-	assert.NotNil(t, info)
-	assert.Nil(t, err)
-	assert.EqualValues(t, typeof(APIResponse{}), typeof(info))
-	assert.EqualValues(t, true, info.Ok)
-	assert.EqualValues(t, "Webhook is already set", info.Description)
-	assert.EqualValues(t, 0, info.ErrorCode)
+		telegram_client := NewClient("CORRECT_TOKEN")
+		telegram_client.HttpClient = &MockClient{
+			GetFunc: func(url string) (*http.Response, error) {
+				r := ioutil.NopCloser(bytes.NewReader([]byte(json_response)))
+				return &http.Response{
+					StatusCode: http.StatusOK,
+					Body:       r,
+				}, nil
+			},
+		}
 
-	assert.EqualValues(t, true, result)
+		// --- try --------
+		url := "https://15ba-217-25-217-143.eu.ngrok.io/tg"
+		info, err := telegram_client.SetWebhook(url)
+		var result bool
+		json.Unmarshal(*info.Result, &result)
+
+		// --- assert --------
+		assert.NotNil(t, info)
+		assert.Nil(t, err)
+		assert.EqualValues(t, typeof(APIResponse{}), typeof(info))
+		assert.EqualValues(t, true, info.Ok)
+		assert.EqualValues(t, "Webhook is already set", info.Description)
+		assert.EqualValues(t, 0, info.ErrorCode)
+
+		assert.EqualValues(t, true, result)
+	})
 }
